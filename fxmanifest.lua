c553ec909456aea9329976c49274e966e907e18f
Lordbeerusza <Ntaliep0@gmail.com>
2024-09-28 22:46:43 +0200

    k

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Lordbeerusza'
-description 'QBCore Integrated Remote Vehicle Control Menu'
+description 'Standalone Vehicle Control Menu'
 version '1.0.0'
 
 client_script 'client.lua'
